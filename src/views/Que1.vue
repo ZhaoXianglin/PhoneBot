e1623ed327e5d765f3095ea658ebe32afe1ec4e9
xianglin <sisivip@vip.qq.com>
2022-08-22 00:13:50 +0800

    questionnaire

--- src/views/Que1.vue
+++ src/views/Que1.vue
@@ -8,37 +8,11 @@
       <van-step>Step4</van-step>
     </van-steps>
     <van-form @submit="onSubmit" scroll-to-error @failed="onFailed" validate-trigger="onSubmit">
-      <p style="padding:0 10px;font-weight:bold;text-align: left">How much do you agree or disagree with the following
-        statements?</p>
-      <p style="padding:0 10px;font-weight:normal; text-align: left;font-size: 14px">Strongly disagree(1), Disagree(2),
-        Somewhat
-        disagree(3), Neutral(4), Somewhat agree(5), Agree(6), Strongly agree(7)</p>
-      <van-field v-for="(item, index) in q1group" :key="item.t" :name="item.t"
-                 :rules="[{ required: auth, message: 'required' }]">
-        <template #input>
-          <van-row style="width: 100%;">
-            <van-row type="flex">
-              <van-col span="24"><span style="font-weight:600">{{ index + 1 }}. {{ item.q }}</span></van-col>
-            </van-row>
-            <van-row type="flex" align="center" justify="between">
-              <van-col span="4" class="score_left_s" style="text-align:right;">Strongly disagree</van-col>
-              <van-col>
-                <van-radio-group v-model="q1groupans[index]" direction="horizontal" class="matrix_table">
-                  <van-radio :name="val" v-for="val in 7" :key="val" checked-color="#ee0a24" class="item">
-                    {{ val }}
-                  </van-radio>
-                </van-radio-group>
-              </van-col>
-              <van-col span="4">Strongly agree</van-col>
-            </van-row>
-          </van-row>
-        </template>
-      </van-field>
       <van-field :rules="[{ required: auth, message: 'required' }]" name="check1">
         <template #input>
           <van-row style="width: 100%;">
             <van-row type="flex">
-              <van-col span="24"><span style="font-weight:600">22. Did the chatbot explain why the phones were recommended?</span>
+              <van-col span="24"><span style="font-weight:600">1. Did the chatbot explain why the phones were recommended?</span>
               </van-col>
             </van-row>
             <van-row type="flex" align="center" justify="between">
@@ -67,7 +41,7 @@
         <template #input>
           <van-row style="width: 100%;">
             <van-row type="flex">
-              <van-col span="24"><span style="font-weight:600">23.Did the chatbot show anyone’s opinions about the recommended phones?</span>
+              <van-col span="24"><span style="font-weight:600">2.Did the chatbot show anyone’s opinions about the recommended phones?</span>
               </van-col>
             </van-row>
             <van-row type="flex" align="center" justify="between">
@@ -93,7 +67,32 @@
         </template>
       </van-field>
 
-
+      <p style="padding:0 10px;font-weight:bold;text-align: left">How much do you agree or disagree with the following
+        statements?</p>
+      <p style="padding:0 10px;font-weight:normal; text-align: left;font-size: 14px">Strongly disagree(1), Disagree(2),
+        Somewhat
+        disagree(3), Neutral(4), Somewhat agree(5), Agree(6), Strongly agree(7)</p>
+      <van-field v-for="(item, index) in q1group" :key="item.t" :name="item.t"
+                 :rules="[{ required: auth, message: 'required' }]">
+        <template #input>
+          <van-row style="width: 100%;">
+            <van-row type="flex">
+              <van-col span="24"><span style="font-weight:600">{{ index + 3 }}. {{ item.q }}</span></van-col>
+            </van-row>
+            <van-row type="flex" align="center" justify="between">
+              <van-col span="4" class="score_left_s" style="text-align:right;">Strongly disagree</van-col>
+              <van-col>
+                <van-radio-group v-model="q1groupans[index]" direction="horizontal" class="matrix_table">
+                  <van-radio :name="val" v-for="val in 7" :key="val" checked-color="#ee0a24" class="item">
+                    {{ val }}
+                  </van-radio>
+                </van-radio-group>
+              </van-col>
+              <van-col span="4">Strongly agree</van-col>
+            </van-row>
+          </van-row>
+        </template>
+      </van-field>
       <div style="margin: 36px;">
         <van-button round block type="info" native-type="submit" :loading="loading">Continue</van-button>
       </div>
@@ -113,6 +112,10 @@ export default {
       auth: true,
       //q_seq: [1, 4, 7, 10, 13, 16, 19, 2, 5, 8, 11, 14, 17, 20, 3, 6, 9, 12, 15, 18, 21],
       q1group: [
+        {q: "The chatbot behaved like a human.", t: "cui_human1"},
+        {q: "I felt like conversing with a real human when interacting with this chatbot.", t: "cui_human2"},
+        {q: "This chatbot system has human properties.", t: "cui_human3"},
+
         {q: "I usually trust people until they give me a reason not to trust them.", t: "trust_propensity1"},
         {q: "Trusting another person is not difficult for me.", t: "trust_propensity2"},
         {q: "My tendency to trust others is high.", t: "trust_propensity3"},
@@ -127,16 +130,12 @@ export default {
         {q: "The chatbot told me the reason why I received the recommended phones.", t: "explain4"},
         {q: "There is a sense of human warmth in this chatbot.", t: "social_presence4"},
 
-
         {q: "Using the recommender to find what I like is easy.", t: "ease4"},
         {q: "The chatbot helped me find the ideal phone.", t: "useful2"},
-        {q: "The chatbot behaved like a human.", t: "cui_human1"},
         {q: "Finding a phone to buy with the help of the recommender is easy.", t: "ease5"},
         {q: "Using the chatbot to find what I like is easy.", t: "useful3"},
-        {q: "I felt like conversing with a real human when interacting with this chatbot.", t: "cui_human2"},
         {q: "It was easy to find what I liked by using the chatbot.", t: "ease6"},
         {q: "Using the chatbot helps me select a mobile phone more quickly.", t: "useful4"},
-        {q: "This chatbot system has human properties.", t: "cui_human3"},
 
       ],
       q1groupans: Array(23).fill(null),
